c5954c64d38ff656b04baf13310345300a638945
abutun11 <butunalican01@gmail.com>
2024-03-23 01:13:53 +0300

    Add project files.

---
+++ Intro/DataAccess/Concretes/DapperCourseDal.cs
@@ -0,0 +1,52 @@
+﻿using Info.Entities;
+using Intro.DataAccess.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.DataAccess.Concretes
+{
+    public class DapperCourseDal : ICourseDal
+    {
+            List<Course> courses;
+            public DapperCourseDal()
+            {
+
+                //Constructor
+                Course course1 = new Course();
+                course1.Id = 1;
+                course1.Name = "Rust";
+                course1.Description = ".NET8";
+                course1.Price = 0;
+
+                Course course2 = new Course();
+                course2.Id = 2;
+                course2.Name = "Java";
+                course2.Description = "JAVA";
+                course2.Price = 10;
+
+                Course course3 = new Course();
+                course3.Id = 3;
+                course3.Name = "Python";
+                course3.Description = "PYTHON 3.12";
+                course3.Price = 100;
+                Console.WriteLine("başladı...");
+
+
+                courses = new List<Course> { course1, course2, course3 };
+
+            }
+
+            public void Add(Course course)
+            {
+                throw new NotImplementedException();
+            }
+
+            public List<Course> GetAll()
+            {
+                return courses;
+            }
+    }
+}
